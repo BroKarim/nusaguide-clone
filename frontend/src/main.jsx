ebf312379e4e3abb5dc8e389d7b6497534fd9e6b
kiramDZ <hilmi0@mhs.unsyiah.ac.id>
2024-06-19 11:17:35 +0700

    add vercel to deploy

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
-import { AuthContextProvider } from './context/AuthContext.jsx'
+import { AuthContextProvider } from './context/AuthContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
